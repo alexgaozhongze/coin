16e5037ab90457c320fe5cbf2b32e52a992c56d2
alex <alexgaozhongze@gmail.com>
2021-03-10 17:34:06 +0800

    go beyond

--- buy.sh
+++ buy.sh
@@ -2,7 +2,7 @@
 ps -ef | grep mix | grep buy | grep -v grep
 if [ $? -ne 0 ]
 then
-/usr/bin/php /data/www/coin/bin/mix.php buy
+/usr/bin/php /data/www/coin/bin/mix.php buy >> /tmp/buy.log
 else
-echo "runing....."
+echo "runing......"
 fi
--- sell.sh
+++ sell.sh
@@ -2,7 +2,7 @@
 ps -ef | grep mix | grep sell | grep -v grep
 if [ $? -ne 0 ]
 then
-/usr/bin/php /data/www/coin/bin/mix.php sell
+/usr/bin/php /data/www/coin/bin/mix.php sell >> /tmp/sell.log
 else
-echo "runing....."
+echo "runing......"
 fi
