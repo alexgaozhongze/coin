9e7d24d89ae60d5dfd4882b377b5be0232794549
alex <alexgaozhongze@gmail.com>
2021-03-12 15:39:02 +0800

    go beyond

--- app/Console/Commands/BuyCommand.php
+++ app/Console/Commands/BuyCommand.php
@@ -42,44 +42,66 @@ class BuyCommand
     public function handle(Channel $chan, $symbol)
     {
         $client = new Client();
-        $response = $client->get("https://api.huobi.pro/market/history/kline?period=1min&size=3&symbol=$symbol")->getBody();
-        $data = json_decode($response, true);
+        $response = $client->get("https://api.huobi.pro/market/history/kline?period=1min&size=36&symbol=$symbol")->getBody();
+        $symbolRes = json_decode($response, true);
+        $symbolList = $symbolRes['data'];
 
-        $allUp = true;
-        foreach ($data['data'] as $value) {
-            $value['close'] <= $value['open'] && $allUp = false;
-        }
-
-        $currentData = reset($data['data']);
-        if ($allUp) {
-            $low = $currentData['low'];
-            foreach ($data['data'] as $value) {
-                $low >= $value['low'] && $low = $value['low'];
+        $symbolList = array_reverse($symbolList);
+        foreach ($symbolList as $key => $value) {
+            if ($key) {
+                $value['ema3']  = 2 / (3  + 1) * $value['close'] + (3  - 1) / (3  + 1) * $symbolList[$key - 1]['ema3'];
+                $value['ema6']  = 2 / (6  + 1) * $value['close'] + (6  - 1) / (6  + 1) * $symbolList[$key - 1]['ema6'];
+                $value['ema9']  = 2 / (9  + 1) * $value['close'] + (9  - 1) / (9  + 1) * $symbolList[$key - 1]['ema9'];
+            } else {
+                $value['ema3'] = $value['close'];
+                $value['ema6'] = $value['close'];
+                $value['ema9'] = $value['close'];
             }
 
-            $up = $currentData['close'] / $low;
-            if (1.03 <= $up) {
-                echo date('Y-m-d H:i:s'), PHP_EOL;
-                echo $symbol, ' ', $low, ' ', $currentData['close'], ' ', $up, PHP_EOL;
+            $symbolList[$key] = $value;
+        }
+
+        $allUp = true;
+        $currentData = end($symbolList);
+        for ($i = 0; $i < 6; $i ++) {
+            $current = current($symbolList);
+            $prev = prev($symbolList);
+            if ($current['open'] >= $prev['open'] || $current['close'] >= $prev['close'] || $current['high'] >= $prev['high'] || $current['low'] >= $prev['low']) {
+                continue;
+            }
+
+            $allUp = false;
+        }
+
+        $macdAllUp = false;
+        $currentData['ema3'] >= $currentData['ema6'] && $currentData['ema6'] >= $currentData['ema9'] && $macdAllUp = true;
+
+        if ($allUp && $macdAllUp) {
+            $redis = context()->get('redis');
+            if ($redis->setnx("$symbol:lock", null)) {
+                echo $symbol, ' ', $currentData['close'], ' ', date('Y-m-d H:i:s'), PHP_EOL;
+
                 $coin = new CoinModel();
                 $buyRes = $coin->place_order(5, 0, $symbol, 'buy-market');
                 if ('ok' == $buyRes->status) {
                     $orderId = $buyRes->data;
 
-                    $redis = context()->get('redis');
                     $redis->lpush("buy:order", $orderId);
-            
-                    $conn = $redis->borrow();
-                    $conn = null;
+                    $redis->setex("$symbol:lock", 666, null);
 
                     echo $buyRes->data, PHP_EOL;
+
                 } else {
+                    $redis->setex("$symbol:lock", 333, null);
+
                     echo $buyRes->{"err-msg"}, PHP_EOL;
-                    sleep(6);
                 }
             }
+            $conn = $redis->borrow();
+            $conn = null;
         }
 
         $chan->push([]);
     }
+
 }
--- app/Console/Workers/SellWorker.php
+++ app/Console/Workers/SellWorker.php
@@ -3,6 +3,7 @@
 namespace App\Console\Workers;
 
 use App\Console\Models\CoinModel;
+use GuzzleHttp\Client;
 use Mix\WorkerPool\AbstractWorker;
 
 /**
@@ -32,55 +33,59 @@ class SellWorker extends AbstractWorker
         if ('ok' == $order->status) {
             $orderInfo = $order->data;
             $symbol = $orderInfo->symbol;
-            $amount = $orderInfo->{"field-amount"} - $orderInfo->{"field-fees"};
-            $createAt = $orderInfo->{"created-at"};
-
-            echo date('Y-m-d H:i:s'), ' ', $symbol, PHP_EOL;
 
             $sell = false;
             while (!$sell) {
-                $kLine = $coin->get_history_kline($symbol, '1min', 2);
-                $prevKLine = end($kLine->data);
-                $curKLine = reset($kLine->data);
+                $client = new Client();
+                $response = $client->get("https://api.huobi.pro/market/history/kline?period=1min&size=36&symbol=$symbol")->getBody();
+                $symbolRes = json_decode($response, true);
+                $symbolList = $symbolRes['data'];
+        
+                $symbolList = array_reverse($symbolList);
+                foreach ($symbolList as $key => $value) {
+                    if ($key) {
+                        $value['ema3']  = 2 / (3  + 1) * $value['close'] + (3  - 1) / (3  + 1) * $symbolList[$key - 1]['ema3'];
+                        $value['ema6']  = 2 / (6  + 1) * $value['close'] + (6  - 1) / (6  + 1) * $symbolList[$key - 1]['ema6'];
+                        $value['ema9']  = 2 / (9  + 1) * $value['close'] + (9  - 1) / (9  + 1) * $symbolList[$key - 1]['ema9'];
+                        $value['ema36'] = 2 / (36 + 1) * $value['close'] + (36 - 1) / (36 + 1) * $symbolList[$key - 1]['ema36'];
+                    } else {
+                        $value['ema3'] = $value['close'];
+                        $value['ema6'] = $value['close'];
+                        $value['ema9'] = $value['close'];
+                        $value['ema36'] = $value['close'];
+                    }
+        
+                    $symbolList[$key] = $value;
+                }
+        
+                $currentData = end($symbolList);
+                if ($currentData['ema3'] < $currentData['ema6'] && $currentData['ema3'] < $currentData['ema9']) {
+                    echo $symbol, ' ', $currentData['close'], ' ', date('Y-m-d H:i:s'), PHP_EOL;
 
-                if ($prevKLine->open > $prevKLine->close) {
+                    $amount = $orderInfo->{"field-amount"} - $orderInfo->{"field-fees"};
+
                     $redis = context()->get('redis');
                     $symbolInfo = $redis->hget('symbol', $symbol);
-    
-                    $conn = $redis->borrow();
-                    $conn = null;
-    
                     $symbolInfo = unserialize($symbolInfo);
         
                     list($int, $float) = explode('.', $amount);
                     $float = substr($float, 0, $symbolInfo['amount-precision']);
                     $amount = "$int.$float";
         
-                    echo 'more', ' ', $symbol, ' ', $curKLine->close, PHP_EOL;
-                    $res = $coin->place_order($amount, 0, $symbol, 'sell-market');
-                    var_dump($res);
-                    $sell = true;
-                } elseif (666 <= time() - $createAt / 1000 && $curKLine->close == $curKLine->high) {
-                    $redis = context()->get('redis');
-                    $symbolInfo = $redis->hget('symbol', $symbol);
+                    $sellRes = $coin->place_order($amount, 0, $symbol, 'sell-market');
+                    if ('ok' == $sellRes->status) {
+                        echo $sellRes->data, PHP_EOL;
 
+                        $redis->del("$symbol:lock");
+                        $sell = true;
+                    } else {
+                        echo $sellRes->{"err-msg"}, PHP_EOL;
+                    }
+    
                     $conn = $redis->borrow();
                     $conn = null;
-    
-                    $symbolInfo = unserialize($symbolInfo);
-
-                    list($int, $float) = explode('.', $amount);
-                    $float = substr($float, 0, $symbolInfo['amount-precision']);
-                    $amount = "$int.$float";
-
-                    echo 'timeout', ' ', $symbol, ' ', $curKLine->close, PHP_EOL;
-                    $res = $coin->place_order($amount, 0, $symbol, 'sell-market');
-                    var_dump($res);
-                    $sell = true;
                 }
             }
-        } else {
-            echo $order->{"err-msg"}, PHP_EOL;
         }
     }
 
