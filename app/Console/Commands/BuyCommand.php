f16a817f49b7499c35a03498e10e7a119298f020
alex <alexgaozhongze@gmail.com>
2021-03-30 18:15:19 +0800

    go beyond

--- app/Console/Commands/BuyCommand.php
+++ app/Console/Commands/BuyCommand.php
@@ -123,7 +123,7 @@ class BuyCommand
         $amount /= $mul;
 
         echo 'buy: ', $symbol, ' ', $price, ' ', $amount, ' ', date('H:i:s', strtotime("+8 hours")), PHP_EOL;
-        $redis->setex("buy:symbol:$symbol", 63, $price);
+        $redis->setex("buy:symbol:$symbol", 96, $price);
         $buyRes = $coin->place_order($amount, $price, $symbol, 'buy-limit');
         if ('ok' == $buyRes->status) {
             echo 'buy: ', $symbol, ' ', $buyRes->data, ' ', date('H:i:s', strtotime("+8 hours")), PHP_EOL;
@@ -136,7 +136,7 @@ class BuyCommand
                 if (!$ts) return;
                 
                 $redis = context()->get('redis');
-                $redis->setex("buy:symbol:$symbol", 63, null);
+                $redis->setex("buy:symbol:$symbol", 96, null);
 
                 $conn = $redis->borrow();
                 $conn = null;
@@ -189,7 +189,7 @@ class BuyCommand
                 return;
             });
         } else {
-            $redis->setex("buy:symbol:$symbol", 63, $price);
+            $redis->setex("buy:symbol:$symbol", 96, $price);
 
             echo $buyRes->{"err-msg"}, PHP_EOL;
         }
